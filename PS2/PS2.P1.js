ff33a0c29cf3d06a3525c6a503bdc70a1449bc90
Philip Huang <phuang98@bu.edu>
2020-06-01 14:48:00 -0400

    complete problem 1

---
+++ PS2/PS2.P1.js
@@ -0,0 +1,29 @@
+//Problem 1
+
+function* fibs () {
+    let [val1, val2, result] = [0, 1, 0];
+    yield 0;
+    yield 1;
+    while(true) {
+        result = val1 + val2;
+        [val1, val2] = [val2, result];
+        // val1 = val2;
+        // val2 = result;
+        yield result;
+    }
+}
+
+let myFibs = fibs();
+let count = 6;
+
+function* even () {
+    let val = myFibs.next().value;
+    if (val % 2 == 0) {
+        console.log(`Even fib: ${val}`);
+        count--;
+    }
+}
+
+while (count > 0){
+    even().next();
+}
