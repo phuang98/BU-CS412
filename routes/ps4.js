f4705c613e63bd2ce50425afc537c51a6e4d7436
Philip Huang <phuang98@bu.edu>
2020-06-21 15:24:47 -0400

    made updates to PS5b

--- routes/ps4.js
+++ routes/ps4.js
@@ -7,6 +7,7 @@ const CONFIG = require('../config/weatherAPI');
 const redis = require('redis');
 const client = redis.createClient();
 
+
 router.route('/current')
     .get(async (req, res, next) => {
         let result = await fetch(CONFIG.url + '?lat=42.36&lon=-71.06&units=imperial&appid=' + CONFIG.key);
@@ -17,22 +18,21 @@ router.route('/current')
         client.exists(name, (err, response) => {
             if(err) {throw new Error(err)}
             if(response == 1) { //key exists, grab value
+                test = JSON.stringify('CACHED');
                 client.get(name, (err, response) => {
-                    console.table(response);
                     console.log(JSON.stringify(name + ' cached'));
                 })
             } else {
-                const reversedName = name.split('').sort().join('');
-                client.set(name, reversedName, (err, reponse) => { //name = key, reversedName = value
-                    console.table(response);
-                    console.log(JSON.stringify(name + ' not in cached'));
+                test = JSON.stringify('NOT IN CACHE');
+                client.set(name, name, (err, reponse) => { //name = key, reversedName = value
+                    console.log(JSON.stringify(name + ' not in cache'));
                 });
             }
         })
         client.expire(name, 30);
 
         res.render('ps4', {title: 'Weather for the Next 3 Hours!', lat: weather.lat, lon: weather.lon,
-            first: weather.hourly[0].temp, second: weather.hourly[1].temp, third: weather.hourly[2].temp});
+            first: weather.hourly[0].temp, second: weather.hourly[1].temp, third: weather.hourly[2].temp, info: test});
     })
     .post(async (req, res, next) => {
         let result = await fetch(CONFIG.url + '?lat=' +  req.body.lat + '&lon=' + req.body.lon
@@ -44,22 +44,21 @@ router.route('/current')
         client.exists(name, (err, response) => {
             if(err) {throw new Error(err)}
             if(response == 1) { //key exists, grab value
+                test = (JSON.stringify('CACHED'));
                 client.get(name, (err, response) => {
-                    console.table(response);
                     console.log(JSON.stringify(name + ' cached'));
                 })
             } else {
-                const reversedName = name.split('').sort().join('');
-                client.set(name, reversedName, (err, reponse) => { //name = key, reversedName = value
-                    console.table(response);
-                    console.log(JSON.stringify(name + ' not in cached'));
+                test = (JSON.stringify('NOT IN CACHE'));
+                client.set(name, name, (err, reponse) => { //name = key, reversedName = value
+                    console.log(JSON.stringify(name + ' not in cache'));
                 });
             }
         })
         client.expire(name, 30);
 
         res.render('ps4', {title: 'Weather for the Next 3 Hours!', lat: weather.lat, lon: weather.lon,
-            first: weather.hourly[0].temp, second: weather.hourly[1].temp, third: weather.hourly[2].temp});
+            first: weather.hourly[0].temp, second: weather.hourly[1].temp, third: weather.hourly[2].temp, info: test});
     })
 
 module.exports = router;
